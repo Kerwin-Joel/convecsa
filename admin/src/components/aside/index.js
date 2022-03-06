b3f185f9a90522daa98729522dc14a14db1a879a
Kerwin <joelss4029@gmail.com>
2022-03-06 00:01:12 -0500

    add architecture

---
+++ admin/src/components/aside/index.js
@@ -0,0 +1,53 @@
+import React from 'react'
+import { Link } from "react-router-dom";
+
+import { AsideContainer,Logo } from './style';
+import { Node } from '../../resources/svgs/Node';
+
+export const Aside = () => {
+  return (
+    <AsideContainer>
+      <Logo>
+        <Node width={30}/>
+        <h3>Convecsa</h3>
+      </Logo>
+      <hr />
+      <div>
+        <ul>
+          <li primary={true}>
+            <Node width={20}/>
+            <Link to="/">Section 1</Link>
+          </li>
+          <li>
+            <Node width={20}/>
+            <Link to="/sectionTwo">Section 2</Link>
+          </li>
+          <li>
+            <Node width={20}/>
+            <a href="google.com">Section 3</a>
+          </li>
+          <li>
+            <Node width={20}/>
+            <a href="google.com">Section 4</a>
+          </li>
+          <li>
+            <Node width={20}/>
+            <a href="google.com">Section 5</a>
+          </li>
+          <li>
+            <Node width={20}/>
+            <a href="google.com">Section 6</a>
+          </li>
+          <li>
+            <Node width={20}/>
+            <a href="google.com">Section 7</a>
+          </li>
+          <li>
+            <Node width={20}/>
+            <a href="google.com">Section 8</a>
+          </li>
+        </ul>
+      </div>
+    </AsideContainer>
+  )
+}
---
+++ admin/src/components/sections/sectionOne/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const SectionOne = () => {
+  return (
+    <div>
+      <h1>Section 1</h1>
+    </div>
+  )
+}
